720a01b8d867d59c5c7a362dab89db2f74dd41a8
shootmyleg <112838308+Destroyedleg@users.noreply.github.com>
2023-01-29 08:19:30 +0530

    Update whitelists.lua

--- whitelists.lua
+++ whitelists.lua
@@ -1,12 +1,9 @@
  shared.loaddata = {4283722044, 4287132134, 4287132134}
-   shared.Funny = {}
-
-   pcall(function()
-   for i,v in pairs(shared.Funny) do
-      if v == game.Players.LocalPlayer.UserId then
-         setfpscap(15)
-      end
-   end
-   end)
-end
-
+shared.Funny = {}
+pcall(function()
+    for i,v in pairs(shared.Funny) do
+        if v == game.Players.LocalPlayer.UserId then
+            setfpscap(15)
+        end
+    end
+end)
