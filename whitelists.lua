12cc86419ff577d7942ba4ed70cb433cf47f8aa2
shootmyleg <112838308+Destroyedleg@users.noreply.github.com>
2023-01-31 16:04:15 +0530

    Delete whitelists.lua

--- whitelists.lua
+++
@@ -1,9 +0,0 @@
-shared.loaddata = {236309920, 459208566, 4236112792, 4236309920, 4287132134}
-shared.Funny = {}
-pcall(function()
-    for i,v in pairs(shared.Funny) do
-        if v == game.Players.LocalPlayer.UserId then
-            setfpscap(15)
-        end
-    end
-end)
